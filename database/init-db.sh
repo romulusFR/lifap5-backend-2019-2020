cc1626b3779f13155933f30d8f4f61eda4f49592
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-08 22:15:03 +0100

    Initialisation DB

---
+++ database/init-db.sh
@@ -0,0 +1,8 @@
+# a executer en temps qu'utilisateur postgres
+# sudo -u postgres -i
+
+createuser --no-createdb --echo --pwprompt lifap5
+createdb --echo --owner lifap5 lifap5 
+
+psql --dbname lifap5 -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+psql --dbname lifap5 -c 'CREATE EXTENSION IF NOT EXISTS "fuzzystrmatch";'
