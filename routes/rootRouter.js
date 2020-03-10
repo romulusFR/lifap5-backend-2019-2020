3339cb23c8eb28a04355f91c8bc55b49fb307677
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-10 21:22:30 +0100

    renommage cohérence

---
+++ routes/rootRouter.js
@@ -0,0 +1,28 @@
+// A default router for examples and tests
+
+const express = require('express');
+const createError = require('http-errors');
+const { logger, config } = require('../utils');
+
+const rootRouter = express.Router();
+
+rootRouter.get('/error', (_req, _res, next) =>
+  next(new createError.NotImplemented('Not yet'))
+);
+
+// basic echo service : simply returns the json body
+// curl -X POST -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" http://localhost:3000/echo/
+rootRouter.post('/echo', function echoHandler(req, res) {
+  logger.debug(`echoHandler(${req.body})`);
+  return res.status(200).send(req.body);
+});
+
+rootRouter.get('/', (_req, res, _next) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <h1>${config.name}@${config.version}</h1>
+  </html>`);
+});
+
+module.exports = rootRouter;
