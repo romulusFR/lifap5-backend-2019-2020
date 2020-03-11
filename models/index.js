4f5cfac7390b30ea637fae387664d7d00c50adfa
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-11 10:11:59 +0100

    Ajout du healthcheck et graceful shutdown

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // all models are exported from this file
 
 const UserDAO = require('./UserDAO');
+const pool = require('./pool');
 
-module.exports = { UserDAO };
+module.exports = { UserDAO, pool };
