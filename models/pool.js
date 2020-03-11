9a1c22bb68abb61adb247f220e93516fa0ae45fd
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-11 09:48:19 +0100

    Contrôle d'erreur pool.on('error', ...)

--- models/pool.js
+++ models/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const { config } = require('../utils');
+const { config, logger } = require('../utils');
 
 const pool = new Pool({
   user: config.pgUser,
@@ -7,6 +7,14 @@ const pool = new Pool({
   database: config.pgDname,
   password: config.pgPass,
   port: config.pgPort,
+  connectionTimeoutMillis: 2000,
+  idleTimeoutMillis: 10000, // default
+  max: 20,
+});
+
+pool.on('error', (err, _client) => {
+  logger.error(`Unexpected error on client with error: ${err}`);
+  throw err;
 });
 
 module.exports = pool;
