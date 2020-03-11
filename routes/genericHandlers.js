5ea90f6fc2f028facb4ee69260f8a464f579a350
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-11 21:51:03 +0100

    Documentation

--- routes/genericHandlers.js
+++ routes/genericHandlers.js
@@ -1,6 +1,16 @@
 const createError = require('http-errors');
 const { logger, config } = require('../utils');
 
+// content negotiation
+// http://expressjs.com/en/api.html#res.format
+// http://expressjs.com/en/api.html#res.render
+// http://expressjs.com/en/guide/using-template-engines.html
+// https://github.com/expressjs/express/blob/master/lib/response.js#L659
+
+// curl -I -H'Accept: application/json' http://localhost:3000/doesnotexists/
+// curl -I -H'Accept: application/json' http://localhost:3000/error/
+// curl -I -H'Accept: text/*' http://localhost:3000/doesnotexists/
+// curl -I -H'Accept: text/*' http://localhost:3000/error/
 function negotiateContentHandler(htmlOpts, jsonOpts) {
   const { view, args } = htmlOpts;
   return (_req, res, _next) => {
@@ -17,25 +27,20 @@ function negotiateContentHandler(htmlOpts, jsonOpts) {
 }
 
 function notFoundHandler(req, res, next) {
-  next(createError.NotFound(`${req.url}`));
+  const msg = `notFoundHandler@${req.method} ${req.url} - ${req.ip}`;
+  logger.debug(msg);
+  next(createError.NotFound(`URL '${req.url}' not found`));
 }
 
-// content negotiation
-// http://expressjs.com/en/api.html#res.format
-// http://expressjs.com/en/api.html#res.render
-// http://expressjs.com/en/guide/using-template-engines.html
-// https://github.com/expressjs/express/blob/master/lib/response.js#L659
+function defaultErrorHandler(err, req, res, _next) {
+  // should NOT happen
+  // if (res.headersSent) {
+  //   next(err);
+  // }
 
-// curl -I -H'Accept: application/json' http://localhost:3000/doesnotexists/
-// curl -I -H'Accept: application/json' http://localhost:3000/error/
-// curl -I -H'Accept: text/*' http://localhost:3000/doesnotexists/
-// curl -I -H'Accept: text/*' http://localhost:3000/error/
-function defaultErrorHandler(err, req, res, next) {
-  if (res.headersSent) {
-    next(err);
-  }
   const { name, message, status = 500 } = err;
   const stack = config.env === 'development' ? err.stack : undefined;
+
   res.status(status);
   res.format({
     html() {
@@ -57,7 +62,7 @@ function defaultErrorHandler(err, req, res, next) {
     },
   });
 
-  const msg = `${status} - ${req.method} ${req.url} - ${req.ip} : ${name} - ${message} @ defaultErrorHandler`;
+  const msg = `defaultErrorHandler@${status} - ${req.method} ${req.url} - ${req.ip} : ${name} - ${message}`;
   if (status >= 500) {
     logger.error(msg);
   } else {
