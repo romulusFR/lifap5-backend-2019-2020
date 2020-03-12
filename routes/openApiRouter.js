39776b45fcbde48fe91211e2d7d5cb903898047d
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-12 22:16:25 +0100

    refactor: swagger-jsdoc, la fausse bonne idée

--- routes/openApiRouter.js
+++ routes/openApiRouter.js
@@ -1,22 +1,37 @@
+/**
+ * @file router for the '/api-docs' roue, bound to swagger-ui-express
+ * @author Romuald THION
+ */
+
 const { Router } = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerDefinition = require('../swagger-definition');
+const YAML = require('yamljs');
 
-const swaggerJSDocOpts = {
-  swaggerDefinition,
-  apis: ['routes/*Router.js'],
-};
 
-const swaggerSpec = swaggerJSDoc(swaggerJSDocOpts);
+// /!\ STOP swagger-jsdoc /!\
 
+// const swaggerJSDoc = require('swagger-jsdoc');
+// const swaggerDefinition = require('../swagger-definition');
+
+// options for swagger-jsdoc: header of OAS and the routes to document
+// const swaggerJSDocOpts = {
+//   swaggerDefinition,
+//   apis: ['routes/rootRouter.js'],
+// };
+// generates the json spec of the API
+// const swaggerSpec = swaggerJSDoc(swaggerJSDocOpts);
+
+// options to swagger-ui-express
+// https://github.com/scottie1984/swagger-ui-express
+// https://github.com/swagger-api/swagger-ui/blob/master/docs/usage/configuration.md
 const swaggerUIOptions = { explorer: true };
+const swaggerDocument = YAML.load('./static/open-api-specification.yaml');
 
+// the router
 const openApiRouter = Router();
-
-openApiRouter.use(
+openApiRouter.use('/', [
   swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, swaggerUIOptions)
-);
+  swaggerUi.setup(swaggerDocument, swaggerUIOptions),
+]);
 
 module.exports = { openApiRouter };
