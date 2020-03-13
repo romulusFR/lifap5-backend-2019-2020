c8eb09c0b2cfcee822f9385caf2aac11f55e913c
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-14 00:49:38 +0100

    feat: quiz POST et PUT

--- models/QuizDAO.js
+++ models/QuizDAO.js
@@ -3,8 +3,47 @@
  * @author Romuald THION
  */
 
+const createError = require('http-errors');
 const { logger } = require('../utils');
 const pool = require('./pool');
+
+const postQuizQuery = `
+INSERT INTO quiz(title, description, owner_id, open)
+VALUES ($1, $2, $3, $4)
+ON CONFLICT (title) DO NOTHING
+RETURNING quiz_id;
+`;
+
+// generatePutQuizQuery({quiz_id : 2, owner_id : 'test.user', open:false});
+
+function generatePutQuizQuery(quiz) {
+  if (!quiz.quiz_id)
+    throw createError.BadRequest(`Invalid content: quiz_id is missing`);
+  if(!quiz.owner_id)
+    throw createError.BadRequest(`Invalid content: owner_id is missing`);
+
+  // keep keys which are updatable and which are defined
+  const updatableAttributes = ['title', 'description', 'open'];
+  const keys = Object.keys(quiz).filter(
+    (key) => updatableAttributes.includes(key) && quiz[`${key}`] !== undefined
+  );
+  if (keys.length === 0)
+    throw createError.BadRequest(`Invalid content: nothing to update`);
+
+  // generate SET attr = $val clauses
+  const templateText = keys.map((key, idx) => `SET ${key} = $${idx}`);
+  // whole query with placeholders
+  const text = `
+  UPDATE quiz
+  ${templateText.join(', ')}
+  WHERE quiz_id = $${keys.length} AND owner_id = $${keys.length + 1}
+  RETURNING *;
+  `;
+
+  // values for placeholders in order
+  const values = keys.map((key) => quiz[`${key}`]).concat([quiz.quiz_id, quiz.owner_id]);
+  return { text, values };
+}
 
 /**
  * @class QuizDAO
@@ -16,6 +55,28 @@ class QuizDAO {
     const result = await pool.query('SELECT * FROM v_quiz_detailed;');
     return result.rows;
   }
+
+  static async postQuiz(quiz) {
+    logger.silly(`postQuiz@${JSON.stringify(quiz)}`);
+    const { title, description, owner, open = false } = quiz;
+    const result = await pool.query(postQuizQuery, [
+      title,
+      description,
+      owner,
+      open,
+    ]);
+    if (result.rowCount) return result.rows[0].quiz_id;
+    throw createError.BadRequest(`Invalid content: title "${quiz.title}" probably already exists (no insertion)`);
+  }
+
+  static async putQuiz(quiz) {
+    logger.silly(`putQuiz@${JSON.stringify(quiz)}`);
+    const result = await pool.query(generatePutQuizQuery(quiz));
+    if (result.rowCount) return result.rows[0];
+    throw createError.BadRequest(`Invalid content ${JSON.stringify(quiz)}`);
+  }
+
+
 }
 
 module.exports = QuizDAO;
--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -4,8 +4,10 @@
  */
 
 const { Router } = require('express');
+const createError = require('http-errors');
 const { logger } = require('../utils');
 const { QuizDAO } = require('../models');
+const { authFromApiKeyHandler } = require('./userRouter');
 // const { negotiateContentHandler } = require('./genericHandlers');
 
 const quizRouter = Router();
@@ -16,11 +18,57 @@ async function getAllQuizzesHandler(_req, res, next) {
     return res.send(results);
   } catch (err) {
     logger.debug(`getAllQuizzesHandler throw ${err}`);
+    return next(err);
+  }
+}
+
+async function postQuizHandler(req, res, next) {
+  try {
+    const { title, description, open } = req.body;
+    const quiz = { title, description, open, owner: res.locals.user.user_id };
+    if (!title)
+      return next(createError.BadRequest(`Invalid content: title is missing`));
+    if (!description)
+      return next(createError.BadRequest(`Invalid content: description is missing`));
+    const quizId = await QuizDAO.postQuiz(quiz);
+    logger.silly(
+      `QuizDAO.postQuiz(${JSON.stringify(quiz)})=${JSON.stringify(quizId)}`
+    );
+    res.send(quizId);
+    return quizId;
+  } catch (err) {
+    logger.debug(`postQuizHandler throw ${err}`);
+    // logger.error(err.stack);
+    return next(err);
+  }
+}
+
+async function putQuizHandler(req, res, next) {
+  try {
+    // eslint-disable-next-line camelcase
+    const { quiz_id, title, description, open } = req.body;
+    const quiz = {
+      quiz_id,
+      title,
+      description,
+      open,
+      owner: res.locals.user.user_id,
+    };
+    const updatedQuiz = await QuizDAO.putQuiz(quiz);
+    logger.silly(`QuizDAO.putQuizHandler(${quiz})=${updatedQuiz}`);
+    res.send(updatedQuiz);
+    return updatedQuiz;
+  } catch (err) {
+    logger.debug(`putQuizHandler throw ${err}`);
     // logger.error(err.stack);
     return next(err);
   }
 }
 
 quizRouter.get('/', [getAllQuizzesHandler]);
+// curl -X POST -H  "Content-Type: application/json" -H "Accept:application/json" -H "X-API-KEY:944c5fdd-af88-47c3-a7d2-5ea3ae3147da"
+quizRouter.post('/', [authFromApiKeyHandler, postQuizHandler]);
+// curl -X PUT -H  "Content-Type: application/json" -H "Accept:application/json" -H "X-API-KEY:944c5fdd-af88-47c3-a7d2-5ea3ae3147da"
+quizRouter.put('/', [authFromApiKeyHandler, putQuizHandler]);
 
 module.exports = { quizRouter };
