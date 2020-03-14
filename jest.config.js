0d429c6e6fe2fc89a0582c7df78edb63ad0f2c7a
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-15 00:41:20 +0100

    test: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   verbose: true,
+  watchman: false, // peut être mis à true, corrigé avec watchman 4.9.0
   testEnvironment: 'node',
   collectCoverage: false,
   collectCoverageFrom: [
-    "/src/**/*.js",
-    "!**/node_modules/**",
-    "!**/vendor/**"
+    'src/**/*.js',
+    '!**/node_modules/**',
   ],
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ]
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: [ "**/tests/**/*.js", "**/?(*.)+(spec|test).js" ],
 };
-
