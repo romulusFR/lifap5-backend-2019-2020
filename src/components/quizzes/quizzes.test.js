c2372ab03b51805b18c6f2504b64ee9942803845
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-16 00:13:09 +0100

    tests: /quizzes/*

--- src/components/quizzes/quizzes.test.js
+++ src/components/quizzes/quizzes.test.js
@@ -65,14 +65,14 @@ describe('GET /quizzes/:quiz_id', () => {
   });
 });
 
-const quizToCreate = {
-  title: `Quiz test #${Math.floor(Math.random() * 1000000)}`,
-  description: 'Description of test quiz',
-  open: true,
-};
-let createdQuizId;
-
 describe('POST /quizzes/', () => {
+  const quizToCreate = {
+    title: `Quiz test #${Math.floor(Math.random() * 1000000)}`,
+    description: 'Description of test quiz',
+    open: true,
+  };
+  let createdQuizId;
+
   it('should create a new quizz', async () => {
     const res = await request(app)
       .post('/quizzes/')
@@ -84,12 +84,14 @@ describe('POST /quizzes/', () => {
     expect(res.statusCode).toEqual(201);
     expect(res.body).toMatchObject({ quiz_id: expect.any(Number) });
 
+    // console.log(`POST createdQuizId=${createdQuizId}`);
     createdQuizId = res.body.quiz_id;
   });
-});
 
-describe('GET /quizzes/:quiz_id (after POST /quizzes/)', () => {
+  
+
   it('should detail the quiz we ve just created', async () => {
+    // console.log(`GET createdQuizId=${createdQuizId}`);
     const gold = {
       quiz_id: createdQuizId,
       owner_id: 'test.user',
@@ -104,37 +106,41 @@ describe('GET /quizzes/:quiz_id (after POST /quizzes/)', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toMatchObject(gold);
   });
-});
 
-describe('PUT /quizzes/:quiz_id (after POST /quizzes/)', () => {
-  it('should update the previous quiz', async () => {
-    const content = {
-      title: `Quiz test #${Math.floor(Math.random() * 1000000)} - modified`,
-      description: 'Description of test quiz - modified',
-      open: false,
-    };
+  describe('PUT /quizzes/:quiz_id (after POST /quizzes/)', () => {
+    // console.log(`PUT createdQuizId=${createdQuizId}`);
+    it('should update the previous quiz', async () => {
+      const content = {
+        title: `Quiz test #${Math.floor(Math.random() * 1000000)} - modified`,
+        description: 'Description of test quiz - modified',
+        open: false,
+      };
 
-    const res = await request(app)
-      .put(`/quizzes/${createdQuizId}`)
-      .set('Accept', 'application/json')
-      .set('X-API-KEY', '944c5fdd-af88-47c3-a7d2-5ea3ae3147da')
-      .send(content) // sends a JSON post body
-      .expect('Content-Type', /json/);
+      const res = await request(app)
+        .put(`/quizzes/${createdQuizId}`)
+        .set('Accept', 'application/json')
+        .set('X-API-KEY', '944c5fdd-af88-47c3-a7d2-5ea3ae3147da')
+        .send(content) // sends a JSON post body
+        .expect('Content-Type', /json/);
 
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toMatchObject(content);
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toMatchObject(content);
+    });
+  });
+
+  describe('DEL /quizzes/:quiz_id (after POST /quizzes/)', () => {
+    // console.log(`DEL createdQuizId=${createdQuizId}`);
+    it('should delete the previous quiz', async () => {
+      const res = await request(app)
+        .delete(`/quizzes/${createdQuizId}`)
+        .set('Accept', 'application/json')
+        .set('X-API-KEY', '944c5fdd-af88-47c3-a7d2-5ea3ae3147da')
+        .expect('Content-Type', /json/);
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toMatchObject({ quiz_id: createdQuizId });
+    });
   });
 });
 
-describe('DEL /quizzes/:quiz_id (after POST /quizzes/)', () => {
-  it('should delete the previous quiz', async () => {
-    const res = await request(app)
-      .delete(`/quizzes/${createdQuizId}`)
-      .set('Accept', 'application/json')
-      .set('X-API-KEY', '944c5fdd-af88-47c3-a7d2-5ea3ae3147da')
-      .expect('Content-Type', /json/);
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toMatchObject({ quiz_id: createdQuizId });
-  });
-});
+// describe('GET /quizzes/:quiz_id (after POST /quizzes/)', () => {});
