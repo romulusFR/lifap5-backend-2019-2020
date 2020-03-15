c8ce859d46b6476715e4831ccc0e9de470f80ac8
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-15 16:52:23 +0100

    test: toutes routes users/

--- src/components/users/users.test.js
+++ src/components/users/users.test.js
@@ -2,22 +2,86 @@
 const request = require('supertest');
 
 const app = require('../../app');
-const { pool }= require('../../config');
+const { pool } = require('../../config');
 
 afterAll((done) => {
   pool.end(() => done());
 });
 
-
 describe('GET /users/', () => {
-  it('should give the list of all user_id\'s', async (done) => {
-    const res = await request(app)
-      .get('/users/')
+  it("should give the list of all user_id's", async (done) => {
+    const res = await request(app).get('/users/');
     expect(res.statusCode).toEqual(200);
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body.length).toBeGreaterThanOrEqual(4);
-    expect(res.body).toContainEqual({user_id: 'test.user'});
-    expect(res.body).toContainEqual({user_id: 'other.user'});
+    expect(res.body).toContainEqual({ user_id: 'test.user' });
+    expect(res.body).toContainEqual({ user_id: 'other.user' });
+    done();
+  });
+});
+
+describe('GET /users/whoami', () => {
+  it('should answer 200 when x-api-key is set with an exiting user', async (done) => {
+    const res = await request(app)
+      .get('/users/whoami')
+      .set('Accept', 'application/json')
+      .set('X-API-KEY', '944c5fdd-af88-47c3-a7d2-5ea3ae3147da')
+      .expect('Content-Type', /json/);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      user_id: 'test.user',
+      firstname: 'Test',
+      lastname: 'User',
+    });
+
+    done();
+  });
+
+  it('should answer 400 when x-api-key is not UUIDv4', async (done) => {
+    const res = await request(app)
+      .get('/users/whoami')
+      .set('Accept', 'application/json')
+      .set('X-API-KEY', '00000000-0000-0000-0000-000000000000')
+      .expect('Content-Type', /json/);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toMatchObject({
+      name: 'BadRequestError',
+      message: expect.any(String),
+      status: 400,
+    });
+    done();
+  });
+
+  it('should answer 401 when no x-api-key is not set', async (done) => {
+    const res = await request(app)
+      .get('/users/whoami')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/);
+
+    expect(res.statusCode).toEqual(401);
+    expect(res.body).toMatchObject({
+      name: 'UnauthorizedError',
+      message: expect.any(String),
+      status: 401,
+    });
+    done();
+  });
+
+  it('should answer 403 when x-api-key is not in the database', async (done) => {
+    const res = await request(app)
+      .get('/users/whoami')
+      .set('Accept', 'application/json')
+      .set('X-API-KEY', '944c5fdd-af88-47c3-a7d2-5ea3ae3147d0')
+      .expect('Content-Type', /json/);
+
+    expect(res.statusCode).toEqual(403);
+    expect(res.body).toMatchObject({
+      name: 'ForbiddenError',
+      message: expect.any(String),
+      status: 403,
+    });
     done();
   });
 });
