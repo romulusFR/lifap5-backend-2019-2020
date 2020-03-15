7880192c84fc6e319733cd52bd22bd3120855552
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-15 10:51:04 +0100

    docs: lien vers page de référence

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
+// SEE
+// https://jestjs.io/docs/en/getting-started
+// https://jestjs.io/docs/en/testing-frameworks
+// https://github.com/visionmedia/supertest
+// https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/
+
 module.exports = {
   verbose: true,
   watchman: false, // peut être mis à true, corrigé avec watchman 4.9.0
