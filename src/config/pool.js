d10c07ad5d27b560c23e9039872f039a73a7c7c9
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-15 10:49:14 +0100

    style: tweak sur node-postgres

--- src/config/pool.js
+++ src/config/pool.js
@@ -7,23 +7,24 @@ const { Pool } = require('pg');
 const config = require('./config');
 const { logger } = require('./logger');
 
+const connectionString = `postgresql://${config.pgUser}:${config.pgPass}@${config.pgHost}:${config.pgPort}/${config.pgDname}`;
+
 const pool = new Pool({
-  user: config.pgUser,
-  host: config.pgHost,
-  database: config.pgDname,
-  password: config.pgPass,
-  port: config.pgPort,
-  connectionTimeoutMillis: 2000,
-  idleTimeoutMillis: 10000, // default
+  connectionString,
+  connectionTimeoutMillis: 2000, // 2 sec
+  idleTimeoutMillis: 300000, // 5 min
   max: 20,
   ssl: { rejectUnauthorized: false },
-  // eslint-disable-next-line camelcase
   application_name: config.appname,
 });
 
 // config postgres
 // ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
 // ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'
+
+pool.on('connect', (_client) => {
+  logger.silly(`Client connected to ${connectionString}`);
+});
 
 pool.on('error', (err, _client) => {
   logger.error(`Unexpected error on client with error: ${err}`);
