9b32ceb8d5137f281862a0826c301b2e30c7bc8f
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-16 18:19:22 +0100

    style: stop require('package.json')

--- src/config/config.js
+++ src/config/config.js
@@ -4,22 +4,24 @@
  */
 
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
-const {
-  name: appname = 'App',
-  version = 'O.O.O',
-  description = 'No description',
-} = require('../../package.json');
+// from package.json
+const appname = process.env.npm_package_name || 'App';
+const version = process.env.npm_package_version || 'O.O.O';
+const description = process.env.npm_package_description || 'No description';
 
-require('dotenv').config({path: path.join(__dirname, "../../.env")});
-
-const pageLimit = Number.parseInt(process.env.PAGE_LIMIT, 10) || 20;
-
+// environment and debug
 const env = process.env.NODE_ENV || 'development';
 const debugLvl = process.env.DEV_CONSOLE_DEBUG_LVL || 'debug';
 
+// http server config
 const httpPort = Number.parseInt(process.env.NODE_PORT, 10) || 3000;
 
+// application configuration
+const pageLimit = Number.parseInt(process.env.PAGE_LIMIT, 10) || 20;
+
+// postgres database configuration
 const pgHost = process.env.PG_HOST || 'localhost';
 const pgPort = Number.parseInt(process.env.PG_PORT, 10) || 5432;
 const pgUser = process.env.PG_USER || 'lifap5';
@@ -27,6 +29,7 @@ const pgPass = process.env.PG_PASS || 'THEPASSWORD';
 const pgDname = process.env.PG_DNAME || 'lifap5';
 const pgSchema = process.env.PG_SCHEMA || 'lifap5';
 
+// exports all variables
 module.exports = {
   appname,
   version,
