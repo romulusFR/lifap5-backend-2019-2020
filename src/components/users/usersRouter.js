d00da1864845bfcacef8d12b5d75f927248d9af4
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-18 17:14:00 +0100

    fix+feat : ownership et details

--- src/components/users/usersRouter.js
+++ src/components/users/usersRouter.js
@@ -14,8 +14,6 @@ const {
 } = require('../../middlewares');
 
 module.exports = function usersRouter(app) {
-  const router = Router();
-
   async function getAllUsersHandler(req, res, next) {
     const page = req.query.page || 1;
     if (!isInt(`${page}`, { min: 1 })) {
@@ -26,7 +24,7 @@ module.exports = function usersRouter(app) {
     }
 
     try {
-      const results = await UserDAO.selectAllUsers(
+      const results = await UserDAO.selectAll(
         Number.parseInt(page, 10),
         app.locals.pageLimit
       );
@@ -43,6 +41,34 @@ module.exports = function usersRouter(app) {
     { jsonArgs: (req, res) => res.locals.user }
   );
 
+  async function checksUserOwnership(req, res, next) {
+    const { user_id } = req.params;
+    logger.silly(`checksUserOwnership@${user_id}`);
+    const whoami = res.locals.user.user_id;
+    if (whoami !== user_id) {
+      const msg = `Cannot access #${user_id} (as ${whoami})`;
+      return next(createError.Forbidden(msg));
+    }
+
+    return next();
+  }
+
+  async function getUserDetails(_req, res, next) {
+    const whoami = res.locals.user.user_id;
+    logger.silly(`getUserDetails@${whoami}`);
+
+    try {
+      const details = await UserDAO.selectById(whoami);
+      return res.send(details);
+    } catch (err) {
+      logger.debug(`getUserDetails throw ${err}`);
+      return next(err);
+    }
+  }
+
+  const router = Router();
+
+ 
   // the list of all users
   // curl -X GET -H "Content-Type:application/json" http://localhost:3000/user/all
   router.get('/', [getAllUsersHandler]);
@@ -55,6 +81,12 @@ module.exports = function usersRouter(app) {
   // 403 (ForbiddenError)   curl -H "Accept:text/*" -H "X-API-KEY:944c5fdd-af88-47c3-a7d2-5ea3ae314700" http://localhost:3000/user/whoami
   // 400 (Bad Request)      curl -H "Accept:text/*" -H "X-API-KEY:invalid" http://localhost:3000/user/whoami
   router.get('/whoami', [authFromApiKeyHandler, whoamiHandler]);
+
+  // /!\ BUG WITH ORDER : router.params comes BEAFORE getUserDetails
+  // when parameter :user_id is used, checks if it owned by current user (res.locals.user)
+  // router.param('user_id', [authFromApiKeyHandler, checksUserOwnership]);
+
+  router.get('/:user_id', [authFromApiKeyHandler, checksUserOwnership, getUserDetails]);
 
   return router;
 };
