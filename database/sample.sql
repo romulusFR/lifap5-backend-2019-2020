44ccf0d1ed632ecf4719552eab659c9054495185
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-18 17:14:37 +0100

    tests: extension exemples

--- database/sample.sql
+++ database/sample.sql
@@ -15,13 +15,21 @@ SELECT setval('lifap5.quiz_quiz_id_seq', 2);
 INSERT INTO lifap5.question(quiz_id, question_id, content)
 VALUES  (0, 0, 'Qui a inventé le lambda calcul ?'),
         (0, 1, 'Qui a inventé le JavaScript ?'),
-        (1, 2, 'En quel année le standard ES2015 a-t''il été proposé ?');
+        (1, 0, 'En quel année le standard ES2015 a-t''il été proposé ?');
 
 
 INSERT INTO lifap5.proposition(quiz_id, question_id, proposition_id, content, correct)
 VALUES  (0, 0, 0, 'Alan Turing', false),
-        (0, 0, 1, 'Alonzo Church', true);
+        (0, 0, 1, 'Alonzo Church', true),
+        (0, 1, 0, 'Brendan Eich', true),
+        (0, 1, 1, 'Obi Wan Kenobi', false),
+        (0, 1, 2, 'Bjarne Stroustrup', false),
+        (1, 0, 0, '2015', true),
+        (1, 0, 1, '2015', false);
 
 
 INSERT INTO lifap5.answer(user_id, quiz_id, question_id, proposition_id)
-VALUES  ('test.user', 0, 0, 1);
+VALUES  ('test.user', 0, 0, 1),
+        ('test.user', 0, 1, 0),
+        ('other.user', 0, 0, 0),
+        ('other.user', 1, 0, 1);
