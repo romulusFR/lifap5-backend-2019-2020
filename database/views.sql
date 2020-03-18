6d727c9f88dd2303a9f20a1b78e4355dea93760e
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-18 09:13:35 +0100

    fix: reprise des vues sans ces horribles fonctions

--- database/views.sql
+++ database/views.sql
@@ -1,85 +1,102 @@
+-- ---------------------------------------- EXTENDED QUIZZES ----------------------------------------
 -- extended quizzes, with aggregation on questions to provide a summary
 DROP VIEW IF EXISTS lifap5.v_quiz_ext;
 CREATE OR REPLACE VIEW lifap5.v_quiz_ext AS(
 
-  WITH questions_array AS(
+  WITH questions_json AS(
     SELECT quiz_id,
-           count(question_id)::integer  as questions_number,
-           -- SUM(weight)::integer as total_weight,
-           jsonb_agg(to_jsonb(question_id)) as questions_ids
+           count(question_id)::integer  AS questions_number,
+           jsonb_agg(to_jsonb(question_id)) AS questions_ids
     FROM question
     GROUP BY quiz_id
   )
 
   SELECT  quiz.*,
-          COALESCE(q.questions_number, 0) as questions_number,
-          -- COALESCE(q.total_weight, 0) as total_weight, 
-          COALESCE(q.questions_ids,'[]') as questions_ids
-  FROM quiz LEFT OUTER JOIN questions_array q
+          COALESCE(q.questions_number, 0) AS questions_number,
+          COALESCE(q.questions_ids,'[]') AS questions_ids
+  FROM quiz LEFT OUTER JOIN questions_json q
     USING (quiz_id)
   ORDER BY quiz.quiz_id
 );
 
 -- select * from v_quiz_ext;
---  quiz_id |          created_at           |     title     |    description    |     owner_id     | open | questions_number | total_weight | questions_ids 
--- ---------+-------------------------------+---------------+-------------------+------------------+------+------------------+--------------+---------------
---        0 | 2020-03-14 21:56:57.124856+01 | QCM LIFAP5 #1 | Des questions ... | romuald.thion    | f    |                2 |            4 | [0, 1]
---        1 | 2020-03-14 21:56:57.124856+01 | QCM LIFAP5 #2 | Des questions ... | romuald.thion    | f    |                1 |            1 | [2]
---        2 | 2020-03-14 21:56:57.124856+01 | QCM LIFAP5 #3 | Des questions ... | emmanuel.coquery | f    |                0 |            0 | []
+--  quiz_id |          created_at           |     title     |             description              |     owner_id     | open | questions_number | questions_ids 
+-- ---------+-------------------------------+---------------+--------------------------------------+------------------+------+------------------+---------------
+--        0 | 2020-03-17 11:05:58.851951+01 | QCM LIFAP5 #1 | Des questions de JS et lambda calcul | romuald.thion    | f    |                2 | [0, 1]
+--        1 | 2020-03-17 11:05:58.851951+01 | QCM LIFAP5 #2 | Des questions de JS et lambda calcul | romuald.thion    | f    |                1 | [2]
+--        2 | 2020-03-17 11:05:58.851951+01 | QCM LIFAP5 #3 | Des questions de JS et lambda calcul | emmanuel.coquery | f    |                0 | []
 
 
+-- ---------------------------------------- EXTENDED QUESTIONS ----------------------------------------
 -- extended questions, with aggregation on propositions to provide a summary
 DROP VIEW IF EXISTS lifap5.v_question_ext;
 CREATE OR REPLACE VIEW lifap5.v_question_ext AS(
-  SELECT  question.*,
-          count(p.proposition_id)::integer as propositions_number,
-          count(p.proposition_id) FILTER (WHERE correct)::integer as correct_propositions_number
-  FROM  question LEFT OUTER JOIN proposition p USING (quiz_id, question_id)
-  GROUP BY quiz_id, question_id
+  WITH propositions_json AS(
+    SELECT quiz_id, question_id,
+           count(proposition_id)::integer AS propositions_number,
+           count(proposition_id) FILTER (WHERE correct)::integer AS correct_propositions_number,
+           jsonb_agg(to_jsonb(proposition_id)) AS propositions_ids
+    FROM proposition
+    GROUP BY quiz_id, question_id
+  )
+  SELECT  q.*,
+          COALESCE(p.propositions_number, 0) AS propositions_number,
+          COALESCE(p.correct_propositions_number, 0) AS correct_propositions_number,
+          COALESCE(p.propositions_ids,'[]') AS propositions_ids
+  FROM  question q LEFT OUTER JOIN propositions_json p
+        USING (quiz_id, question_id)
 );
 
-
--- tool : transforms [NULL] into []
-CREATE OR REPLACE FUNCTION lifap5.jsonb_array_with_null_keys(jsonb)
-RETURNS jsonb AS 
-$$
-  SELECT COALESCE(NULLIF(jsonb_strip_nulls($1), '[{}]'), '[]');
-$$
-LANGUAGE SQL IMMUTABLE;
+-- select * from v_question_ext;
+--  quiz_id | question_id |                        content                        | propositions_number | correct_propositions_number | propositions_ids 
+-- ---------+-------------+-------------------------------------------------------+---------------------+-----------------------------+------------------
+--        0 |           0 | Qui a inventé le lambda calcul ?                      |                   2 |                           1 | [0, 1]
+--        0 |           1 | Qui a inventé le JavaScript ?                         |                   0 |                           0 | []
+--        1 |           2 | En quel année le standard ES2015 a-t'il été proposé ? |                   0 |                           0 | []
 
 
+-- ---------------------------------------- DETAILED QUESTIONS ----------------------------------------
 
 -- detailed questions, with fully detailed propositions in nested json objects
 CREATE OR REPLACE VIEW lifap5.v_question_detailed AS(
-  WITH answers_array AS(
+
+  WITH answers_json AS(
     SELECT  quiz_id, question_id, proposition_id,
             jsonb_agg(jsonb_build_object(
               'user_id', a.user_id,
               'answered_at', a.answered_at
-            )) as answers
+            )) AS answers
     FROM answer a
     GROUP BY quiz_id, question_id, proposition_id
-    )
+    ),
 
-  SELECT  question.*,
-          jsonb_array_with_null_keys(jsonb_agg(
-            jsonb_build_object(
-            'proposition_id', p.proposition_id,
-            'content', p.content,
-            'correct', p.correct,
-            'answers', a.answers
-             ))) as propositions
-  FROM  question LEFT OUTER JOIN proposition p USING (quiz_id, question_id)
-                 LEFT OUTER JOIN answers_array a USING (quiz_id, question_id, proposition_id)
-  GROUP BY quiz_id, question_id
+  propositions_json AS(
+    SELECT  quiz_id, question_id,
+            jsonb_agg(jsonb_build_object(
+              'proposition_id', p.proposition_id,
+              'content', p.content,
+              'correct', p.correct,
+              'answers', COALESCE(a.answers,'[]')
+            )) as propositions
+    FROM proposition p LEFT OUTER JOIN answers_json a USING (quiz_id, question_id, proposition_id)
+    GROUP BY quiz_id, question_id
+  )
+
+  SELECT  q.*,
+          COALESCE(p.propositions,'[]') AS propositions
+
+  FROM  question q LEFT OUTER JOIN propositions_json p USING (quiz_id, question_id)
   ORDER BY quiz_id, question_id
 );
+
 
 -- select quiz_id, question_id, jsonb_pretty(propositions) from v_question_detailed ;
 --  quiz_id | question_id |                           jsonb_pretty                            
 -- ---------+-------------+-------------------------------------------------------------------
 --        0 |           0 | [                                                                +
 --          |             |     {                                                            +
+--          |             |         "answers": [                                             +
+--          |             |         ],                                                       +
 --          |             |         "content": "Alan Turing",                                +
 --          |             |         "correct": false,                                        +
 --          |             |         "proposition_id": 0                                      +
@@ -88,7 +105,18 @@ CREATE OR REPLACE VIEW lifap5.v_question_detailed AS(
 --          |             |         "answers": [                                             +
 --          |             |             {                                                    +
 --          |             |                 "user_id": "test.user",                          +
---          |             |                 "answered_at": "2020-03-14T21:56:57.178206+01:00"+
+--          |             |                 "answered_at": "2020-03-17T11:05:58.907769+01:00"+
+--          |             |             }                                                    +
+--          |             |         ],                                                       +
+--          |             |         "content": "Alonzo Church",                              +
+--          |             |         "correct": true,                                         +
+--          |             |         "proposition_id": 0                                      +
+--          |             |     },                                                           +
+--          |             |     {                                                            +
+--          |             |         "answers": [                                             +
+--          |             |             {                                                    +
+--          |             |                 "user_id": "test.user",                          +
+--          |             |                 "answered_at": "2020-03-17T11:05:58.907769+01:00"+
 --          |             |             }                                                    +
 --          |             |         ],                                                       +
 --          |             |         "content": "Alonzo Church",                              +
@@ -100,4 +128,3 @@ CREATE OR REPLACE VIEW lifap5.v_question_detailed AS(
 --          |             | ]
 --        1 |           2 | [                                                                +
 --          |             | ]
--- (3 rows)
