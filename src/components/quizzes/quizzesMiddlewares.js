956b130a8fec3cdd73541b61e05075dfd5a7206f
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-18 14:52:30 +0100

    style:

--- src/components/quizzes/quizzesMiddlewares.js
+++ src/components/quizzes/quizzesMiddlewares.js
@@ -4,13 +4,11 @@ const { logger } = require('../../config');
 function checksQuizOwnership(_req, res, next) {
   const user = res.locals.user.user_id;
   const owner = res.locals.quiz.owner_id;
-  logger.silly(`checkQuizIdExists@${user} VS ${owner}`);
-  if (user !== owner)
-    return next(
-      createError.Forbidden(
-        `Quiz #${res.locals.quiz.quiz_id} is not owned by ${user} (owner is ${owner})`
-      )
-    );
+  logger.silly(`checksQuizOwnership@${user} VS ${owner}`);
+  if (user !== owner) {
+    const msg = `Quiz #${res.locals.quiz.quiz_id} is not owned by ${user} (owner is ${owner})`;
+    return next(createError.Forbidden(msg));
+  }
   return next();
 }
 
