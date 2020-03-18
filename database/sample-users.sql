57bafde726ba68823d8ed0859605fa2ce836fb09
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-18 10:01:24 +0100

    docs: ajout exemple generation users

--- database/sample-users.sql
+++ database/sample-users.sql
@@ -15,3 +15,13 @@ VALUES  ('romuald.thion',     'Romuald',  'Thion',    '4dd729fd-4709-427f-b371-9
         ('emmanuel.coquery',  'Emmanuel', 'Coquery',  '7038e76c-7fc3-423f-bfaa-97a0872bdb68'),
         ('test.user',         'Test',     'User',     '944c5fdd-af88-47c3-a7d2-5ea3ae3147da'),
         ('other.user',        'Other',    'User',     '64decee2-acca-4a86-8e60-a46c4ccbca97');
+
+
+-- inserts 250 users with random names
+-- INSERT INTO lifap5.quiz_user(user_id, firstname, lastname) (
+--   SELECT  'User #' || r.n::text,
+--           'first' || r.n::text,
+--           'last' || r.n::text
+--   FROM generate_series(1, 250) AS g(i), LATERAL (SELECT floor(random()*1000000) AS n, i) AS r
+-- )
+-- ON CONFLICT DO NOTHING;
