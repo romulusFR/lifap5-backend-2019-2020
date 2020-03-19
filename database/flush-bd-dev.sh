24efb061e3e05df403ecddd5c0c4cff864abd0b6
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-19 17:55:15 +0100

    test:

--- database/flush-bd-dev.sh
+++ database/flush-bd-dev.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo Running in \'"$DIR"\'
+
 PGPORT=5433
 PGHOST=localhost
 PGUSER=lifap5
 
-psql -U "$PGUSER" -h "$PGHOST" -p "$PGPORT" -f ./schema.sql
-psql -U "$PGUSER" -h "$PGHOST" -p "$PGPORT" -f ./views.sql
-psql -U "$PGUSER" -h "$PGHOST" -p "$PGPORT" -f ./sample-users.sql
-psql -U "$PGUSER" -h "$PGHOST" -p "$PGPORT" -f ./sample.sql
+CMD="psql -X -U $PGUSER -h $PGHOST -p $PGPORT"
+
+eval ${CMD} -f $DIR/schema.sql
+eval ${CMD} -f $DIR/views.sql
+eval ${CMD} -f $DIR/sample-users.sql
+eval ${CMD} -f $DIR/sample.sql
 
