534e33797aeb711792c9f62c8f460a5e5e5a4f5e
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-20 14:28:35 +0100

    feat: post/del answers

--- src/components/quizzes/questions/questionsRouter.js
+++ src/components/quizzes/questions/questionsRouter.js
@@ -9,6 +9,7 @@ const { logger } = require('../../../config');
 const { authFromApiKeyHandler } = require('../../../middlewares');
 const { checksQuizOwnership } = require('../quizzesMiddlewares');
 const QuestionDAO = require('./QuestionDAO');
+const PropositionDAO = require('./PropositionDAO');
 
 module.exports = function questionsRouter(_app) {
   const router = Router();
@@ -32,7 +33,7 @@ module.exports = function questionsRouter(_app) {
     logger.silly(`checksQuestionByIdHandler@${question_id}`);
     if (Number.isNaN(parseInt(question_id, 10)))
     return next(
-      createError.BadRequest(`Invalid content: question_id is not an integer`)
+      createError.BadRequest(`Invalid content:' ${question_id}' is not an integer`)
     );
     const { quiz_id } = res.locals.quiz;
     try {
@@ -47,6 +48,31 @@ module.exports = function questionsRouter(_app) {
       return next();
     } catch (err) {
       logger.debug(`checksQuestionByIdHandler throw ${err}`);
+      return next(err);
+    }
+  }
+
+
+  async function checksPropositionByIdHandler(_req, res, next, proposition_id) {
+    logger.silly(`checksPropositionByIdHandler@${proposition_id}`);
+    const { question_id } = res.locals.question;
+    if (Number.isNaN(parseInt(proposition_id, 10)))
+    return next(
+      createError.BadRequest(`Invalid content: '${proposition_id}' is not an integer`)
+    );
+    const { quiz_id } = res.locals.quiz;
+    try {
+      const proposition = await PropositionDAO.selectById(quiz_id, question_id, proposition_id);
+      if (!proposition)
+        return next(
+          createError.NotFound(
+            `Proposition #${proposition_id} of question #${question_id} for quiz ${quiz_id} does not exist`
+          )
+        );
+      res.locals.proposition = proposition;
+      return next();
+    } catch (err) {
+      logger.debug(`checksPropositionByIdHandler throw ${err}`);
       return next(err);
     }
   }
@@ -102,10 +128,42 @@ module.exports = function questionsRouter(_app) {
     try {
       const deletedQuiz = await QuestionDAO.del(quiz_id, question_id);
       logger.silly(`delQuestionHandler@${deletedQuiz}`);
-      res.send(deletedQuiz);
-      return deletedQuiz;
+      return res.status(200).send(deletedQuiz);
+
     } catch (err) {
       logger.debug(`delQuestionHandler throw ${err}`);
+      return next(err);
+    }
+  }
+
+
+  async function postAnswerHandler(req, res, next) {
+    const { user_id } = res.locals.user;
+    const { quiz_id } = res.locals.quiz;
+    const { question_id } = res.locals.question;
+    const { proposition_id } = res.locals.proposition;
+    try {
+      const answer = await PropositionDAO.upsert(user_id, quiz_id, question_id, proposition_id);
+      logger.silly(`postAnswerHandler@${answer}`);
+      return res.status(201).send(answer);
+
+    } catch (err) {
+      logger.debug(`postAnswerHandler throw ${err}`);
+      return next(err);
+    }
+  }
+
+  async function deleteAnswerHandler(req, res, next) {
+    const { user_id } = res.locals.user;
+    const { quiz_id } = res.locals.quiz;
+    const { question_id } = res.locals.question;
+    try {
+      const answer = await PropositionDAO.del(user_id, quiz_id, question_id);
+      logger.silly(`deleteAnswerHandler@${answer}`);
+      return res.status(201).send(answer);
+
+    } catch (err) {
+      logger.debug(`deleteAnswerHandler throw ${err}`);
       return next(err);
     }
   }
@@ -113,7 +171,11 @@ module.exports = function questionsRouter(_app) {
   // when parameter :question_id is used, checks if it exists
   router.param('question_id', checksQuestionByIdHandler);
 
-  //
+  // when parameter :proposition_id is used, checks if it exists
+  router.param('proposition_id', checksPropositionByIdHandler);
+
+
+  // root : all questions
   router.get('/', [getAllQuestionsHandler]);
 
   router.get('/:question_id', [
@@ -143,6 +205,18 @@ module.exports = function questionsRouter(_app) {
     authFromApiKeyHandler,
     checksQuizOwnership,
     delQuestionHandler,
+  ]);
+
+
+  // curl -X POST "http://localhost:3000/quizzes/0/questions/0/answer/0" -H  "accept: application/json" -H  "X-API-KEY: 944c5fdd-af88-47c3-a7d2-5ea3ae3147da" -H  "Content-Type: application/json"
+  router.post('/:question_id/answer/:proposition_id/', [
+    authFromApiKeyHandler,
+    postAnswerHandler,
+  ]);
+
+  router.delete('/:question_id/answer/', [
+    authFromApiKeyHandler,
+    deleteAnswerHandler,
   ]);
 
   return router;
