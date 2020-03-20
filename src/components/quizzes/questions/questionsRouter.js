c6d312049e615726dc802040289cce0fc0e16bc3
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2020-03-20 15:19:39 +0100

    docs:

--- src/components/quizzes/questions/questionsRouter.js
+++ src/components/quizzes/questions/questionsRouter.js
@@ -32,9 +32,11 @@ module.exports = function questionsRouter(_app) {
   async function checksQuestionByIdHandler(_req, res, next, question_id) {
     logger.silly(`checksQuestionByIdHandler@${question_id}`);
     if (Number.isNaN(parseInt(question_id, 10)))
-    return next(
-      createError.BadRequest(`Invalid content:' ${question_id}' is not an integer`)
-    );
+      return next(
+        createError.BadRequest(
+          `Invalid content:' ${question_id}' is not an integer`
+        )
+      );
     const { quiz_id } = res.locals.quiz;
     try {
       const question = await QuestionDAO.selectById(quiz_id, question_id);
@@ -52,17 +54,22 @@ module.exports = function questionsRouter(_app) {
     }
   }
 
-
   async function checksPropositionByIdHandler(_req, res, next, proposition_id) {
     logger.silly(`checksPropositionByIdHandler@${proposition_id}`);
     const { question_id } = res.locals.question;
     if (Number.isNaN(parseInt(proposition_id, 10)))
-    return next(
-      createError.BadRequest(`Invalid content: '${proposition_id}' is not an integer`)
-    );
+      return next(
+        createError.BadRequest(
+          `Invalid content: '${proposition_id}' is not an integer`
+        )
+      );
     const { quiz_id } = res.locals.quiz;
     try {
-      const proposition = await PropositionDAO.selectById(quiz_id, question_id, proposition_id);
+      const proposition = await PropositionDAO.selectById(
+        quiz_id,
+        question_id,
+        proposition_id
+      );
       if (!proposition)
         return next(
           createError.NotFound(
@@ -77,7 +84,7 @@ module.exports = function questionsRouter(_app) {
     }
   }
 
-  function validateQuestion (req, _res, next) {
+  function validateQuestion(req, _res, next) {
     const { sentence, propositions } = req.body;
     if (!sentence)
       return next(
@@ -87,7 +94,7 @@ module.exports = function questionsRouter(_app) {
       return next(
         createError.BadRequest(`Invalid content: propositions is not an array`)
       );
-    
+
     // @todo : checks details of propositions
     return next();
   }
@@ -110,7 +117,6 @@ module.exports = function questionsRouter(_app) {
     const { quiz_id } = res.locals.quiz;
     const { question_id } = res.locals.question;
     try {
-
       const { sentence, propositions } = req.body;
       const question = { quiz_id, question_id, sentence, propositions };
       const questionId = await QuestionDAO.update(question);
@@ -129,13 +135,11 @@ module.exports = function questionsRouter(_app) {
       const deletedQuiz = await QuestionDAO.del(quiz_id, question_id);
       logger.silly(`delQuestionHandler@${deletedQuiz}`);
       return res.status(200).send(deletedQuiz);
-
     } catch (err) {
       logger.debug(`delQuestionHandler throw ${err}`);
       return next(err);
     }
   }
-
 
   async function postAnswerHandler(req, res, next) {
     const { user_id } = res.locals.user;
@@ -143,10 +147,14 @@ module.exports = function questionsRouter(_app) {
     const { question_id } = res.locals.question;
     const { proposition_id } = res.locals.proposition;
     try {
-      const answer = await PropositionDAO.upsert(user_id, quiz_id, question_id, proposition_id);
+      const answer = await PropositionDAO.upsert(
+        user_id,
+        quiz_id,
+        question_id,
+        proposition_id
+      );
       logger.silly(`postAnswerHandler@${answer}`);
       return res.status(201).send(answer);
-
     } catch (err) {
       logger.debug(`postAnswerHandler throw ${err}`);
       return next(err);
@@ -160,8 +168,14 @@ module.exports = function questionsRouter(_app) {
     try {
       const answer = await PropositionDAO.del(user_id, quiz_id, question_id);
       logger.silly(`deleteAnswerHandler@${answer}`);
-      return res.status(201).send(answer);
 
+      if (!answer)
+        return next(
+          createError.NotFound(
+            `Answer to question #${question_id} for quiz ${quiz_id} for user ${user_id} does not exist`
+          )
+        );
+      return res.status(200).send(answer);
     } catch (err) {
       logger.debug(`deleteAnswerHandler throw ${err}`);
       return next(err);
@@ -173,7 +187,6 @@ module.exports = function questionsRouter(_app) {
 
   // when parameter :proposition_id is used, checks if it exists
   router.param('proposition_id', checksPropositionByIdHandler);
-
 
   // root : all questions
   router.get('/', [getAllQuestionsHandler]);
@@ -207,13 +220,13 @@ module.exports = function questionsRouter(_app) {
     delQuestionHandler,
   ]);
 
-
   // curl -X POST "http://localhost:3000/quizzes/0/questions/0/answer/0" -H  "accept: application/json" -H  "X-API-KEY: 944c5fdd-af88-47c3-a7d2-5ea3ae3147da" -H  "Content-Type: application/json"
   router.post('/:question_id/answer/:proposition_id/', [
     authFromApiKeyHandler,
     postAnswerHandler,
   ]);
 
+  // curl -X DELETE "http://localhost:3000/quizzes/0/questions/0/answer/" -H  "accept: application/json" -H  "X-API-KEY: 944c5fdd-af88-47c3-a7d2-5ea3ae3147da" -H  "Content-Type: application/json"
   router.delete('/:question_id/answer/', [
     authFromApiKeyHandler,
     deleteAnswerHandler,
